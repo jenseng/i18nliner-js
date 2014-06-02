87a3cfd860289aed5da8e65892d5f96609c89fd4
Jon Jensen <jenseng@gmail.com>
2014-06-01 20:59:13 -0600

    make lib exports more useful (and not broken)

--- dist/lib/main.js
+++ dist/lib/main.js
@@ -1,7 +1,11 @@
 "use strict";
 var I18nliner = require("./i18nliner")["default"] || require("./i18nliner");
-var i18nJsExtension = require("./extension/i18n_js")["default"] || require("./extension/i18n_js");
+var CallHelpers = require("./call_helpers")["default"] || require("./call_helpers");
+var Errors = require("./errors")["default"] || require("./errors");
+var TranslateCall = require("./extractors/translate_call")["default"] || require("./extractors/translate_call");
 
-I18nliner.extendI18nJs = i18nJsExtension;
+I18nliner.CallHelpers = CallHelpers;
+I18nliner.Errors = Errors;
+I18nliner.TranslateCall = TranslateCall;
 
 exports["default"] = I18nliner;
