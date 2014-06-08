27aa038f465c5cf6b4c986254639bcd6cd7b5636
Jon Jensen <jenseng@gmail.com>
2014-06-07 22:54:59 -0700

    fix some check/export command bugs

--- tasks/i18nliner.js
+++ tasks/i18nliner.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  var Commands = require('../dist/lib/commands');
+  var Commands = require('../dist/lib/commands')['default'];
   grunt.registerTask('i18nliner', function(command) {
     Commands.run(command);
   });
