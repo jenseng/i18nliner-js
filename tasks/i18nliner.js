5c576fcaffe9c137b2a06ec4204e2c36d62f4f1e
Jon Jensen <jenseng@gmail.com>
2014-10-12 13:39:26 -0600

    don't return success on failure, closes #4

--- tasks/i18nliner.js
+++ tasks/i18nliner.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   var Commands = require('../dist/lib/commands')['default'];
   grunt.registerTask('i18nliner', function(command) {
-    Commands.run(command);
+    Commands.run(command) || grunt.fail.fatal(command + " command encountered 1 or more errors");
   });
 };
