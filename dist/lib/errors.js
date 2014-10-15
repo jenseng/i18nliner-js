492eddbb92cdf5072049c66d0a1af1faf1d06e5a
Jon Jensen <jenseng@gmail.com>
2014-10-15 14:32:35 -0600

    make it easy to add custom errors

--- dist/lib/errors.js
+++ dist/lib/errors.js
@@ -7,36 +7,37 @@ function wordify(string) {
   }).trim();
 }
 
-var Errors = {};
-function addError(name) {
-  Errors[name] = function(line, details) {
-    this.line = line;
-    if (details) {
-      var parts = [];
-      var part;
-      if (typeof details === "string" || !details.length) details = [details];
-      for (var i = 0; i < details.length; i++) {
-        part = details[i];
-        part = part === CallHelpers.UNSUPPORTED_EXPRESSION ?
-          "<unsupported expression>" :
-          JSON.stringify(part);
-        parts.push(part);
+var Errors = {
+  register: function(name) {
+    this[name] = function(line, details) {
+      this.line = line;
+      if (details) {
+        var parts = [];
+        var part;
+        if (typeof details === "string" || !details.length) details = [details];
+        for (var i = 0; i < details.length; i++) {
+          part = details[i];
+          part = part === CallHelpers.UNSUPPORTED_EXPRESSION ?
+            "<unsupported expression>" :
+            JSON.stringify(part);
+          parts.push(part);
+        }
+        details = parts.join(', ');
       }
-      details = parts.join(', ');
-    }
-    this.name = name;
-    this.message = wordify(name) + " on line " + line + (details ? ": " + details : "");
-  };
-}
+      this.name = name;
+      this.message = wordify(name) + " on line " + line + (details ? ": " + details : "");
+    };
+  }
+};
 
-addError('InvalidSignature');
-addError('InvalidPluralizationKey');
-addError('MissingPluralizationKey');
-addError('InvalidPluralizationDefault');
-addError('MissingInterpolationValue');
-addError('MissingCountValue');
-addError('InvalidOptionKey');
-addError('KeyAsScope');
-addError('KeyInUse');
+Errors.register('InvalidSignature');
+Errors.register('InvalidPluralizationKey');
+Errors.register('MissingPluralizationKey');
+Errors.register('InvalidPluralizationDefault');
+Errors.register('MissingInterpolationValue');
+Errors.register('MissingCountValue');
+Errors.register('InvalidOptionKey');
+Errors.register('KeyAsScope');
+Errors.register('KeyInUse');
 
 exports["default"] = Errors;
