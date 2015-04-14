0d740aa23d67a4e2ffaa4d4ad49e50bad7970429
Jon Jensen <jenseng@gmail.com>
2015-04-13 22:03:19 -0600

    fix grunt task and bin to 1. work again and 2. use .i18nrc

--- tasks/i18nliner.js
+++ tasks/i18nliner.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  var Commands = require('../dist/lib/commands')['default'];
+  var Commands = require('../dist/lib/main').Commands;
   grunt.registerTask('i18nliner', function(command) {
     Commands.run(command) || grunt.fail.fatal(command + " command encountered 1 or more errors");
   });
